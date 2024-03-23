c014452406b59330cde6d096b40636e544b3a0e3
yuda-lyu <firsemisphere@gmail.com>
2024-03-23 21:21:47 +0800

    modify: test for win

--- test/all.test.mjs
+++ test/all.test.mjs
@@ -1,4 +1,10 @@
 import assert from 'assert'
+// import WDwloadM3u8 from '../src/WDwloadM3u8.mjs'
+
+
+// function isWindows() {
+//     return process.platform === 'win32'
+// }
 
 
 describe('all', function() {
